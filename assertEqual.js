4a7e8ce02c4b25db8ca4e2d5f496f5e1c428501c
Michael <Michael.James.Ashworth@Gmail.com>
2021-11-22 22:26:49 +0000

    Updated function to use template literals and added a Newline character to make the code more readable

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("🚗💨💨 Assertion Passed:" + actual + " === " + expected);
+    console.log(`🚗💨💨 Assertion Passed: ${actual} === ${expected}\n`);
   } else {
-    console.log("🤬🤬🤬 Assertion Failed:" + actual + " !== " + expected);
+    console.log(`🤬🤬🤬 Assertion Failed: ${actual} !== ${expected}\n`);
   }
 };
 
