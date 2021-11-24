c568e1fa614deacd2c2a511ccb7f3481e956f6f2
Michael <Michael.James.Ashworth@Gmail.com>
2021-11-24 23:59:56 +0000

    Working code

---
+++ countOnly.js
@@ -0,0 +1,44 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🚗💨💨 Assertion Passed: ${actual} === ${expected}\n`);
+  } else {
+    console.log(`🤬🤬🤬 Assertion Failed: ${actual} !== ${expected}\n`);
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+  for (const item of allItems) { // let i = 0; i > allItem.length; i++
+    if (itemsToCount[item]) { //this is saying if
+      if (results[item]) { //This is saying if results has a key value that exists, execute the following code
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+//Solving Logic
+//take in items
+//return counts for specific subset of items
+//items = string
