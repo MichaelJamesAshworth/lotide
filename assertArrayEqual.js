f312ae36ee526d87909418e0b7ebbe9e63d7dd63
Michael <Michael.James.Ashworth@Gmail.com>
2021-11-24 04:35:30 +0000

    Working code

---
+++ assertArrayEqual.js
@@ -0,0 +1,24 @@
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  } for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (arrayOne, arrayTwo) {
+  const result = eqArrays (arrayOne, arrayTwo);
+  if (result) {
+    console.log(`🚗💨💨 Assertion Passed: ${arrayOne} === ${arrayTwo}\n`);
+  } else {
+    console.log(`🤬🤬🤬 Assertion Failed: ${arrayOne} !== ${arrayTwo}\n`);
+  }
+};
+
+assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 4]);
+assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 6]);
+assertArraysEqual([1, 2, 3, 4], [1, 2, 3,]);
+
--- eqArrays.js
+++ eqArrays.js
@@ -18,7 +18,7 @@ const eqArrays = function(arrayOne, arrayTwo) {
 };
 
 //test code
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true ); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays([1, 2, 3], [3, 2, 3]), true); // => should FAIL
 assertEqual(eqArrays([], []), true); // => should PASS
 
