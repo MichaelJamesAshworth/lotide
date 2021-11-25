dbbc75ef3369df7c63ff1914b10b48e70821acb2
Michael <Michael.James.Ashworth@Gmail.com>
2021-11-25 23:07:41 +0000

    linted code and fixed errors

--- head.js
+++ head.js
@@ -14,4 +14,4 @@ assertEqual(head([5,6,7]), 5); //multiple elements that are numbers
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); //multiple elements that are strings
 assertEqual(head([3]), 3); // One element, consisting of a number
 assertEqual(head(["one"]), "one"); //One element, consisting of a string
-assertEqual(head([]), ); // Empty array
+assertEqual(head([]),); // Empty array
