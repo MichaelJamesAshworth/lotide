3279bc26e22d6858c29628b4c1dcf5a56c992740
Michael <Michael.James.Ashworth@Gmail.com>
2021-11-25 23:03:49 +0000

    Linted code and fixed errors.

--- map.js
+++ map.js
@@ -9,8 +9,8 @@ const eqArrays = function(arrayOne, arrayTwo) {
   return true;
 };
 
-const assertArraysEqual = function (arrayOne, arrayTwo) {
-  const result = eqArrays (arrayOne, arrayTwo);
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  const result = eqArrays(arrayOne, arrayTwo);
   if (result) {
     console.log(`🚗💨💨 Assertion Passed: ${arrayOne} === ${arrayTwo}\n`);
   } else {
@@ -20,17 +20,17 @@ const assertArraysEqual = function (arrayOne, arrayTwo) {
 
 const words = ["ground", "control", "to", "major", "tom"];
 
-const map = function (arrToMap, callBack) {
+const map = function(arrToMap, callBack) {
   const results = [];
   for (let item of arrToMap) {
     results.push(callBack(item));
-  }  
+  }
   return results;
-}
+};
 const results1 = map(words, word => word[0]);
 const results2 = map(words, word => word.length);
 const results3 = map(words, word => word.length * 2);
 
-assertArraysEqual( results1, [ 'g', 'c', 't', 'm', 't' ]);
-assertArraysEqual( results2, [6, 7, 2, 5, 3]);
-assertArraysEqual( results3, [12, 14, 4, 10, 6])
+assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]);
+assertArraysEqual(results2, [6, 7, 2, 5, 3]);
+assertArraysEqual(results3, [12, 14, 4, 10, 6]);
