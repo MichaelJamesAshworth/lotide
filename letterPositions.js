8c76e96831e34b84a0a7b8050e3e6e821b5b7ffc
Michael <Michael.James.Ashworth@Gmail.com>
2021-11-25 02:59:57 +0000

    Working Code. Linted. Left test code at bottom for future refactoring+testing

---
+++ letterPositions.js
@@ -0,0 +1,43 @@
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  } for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  const result = eqArrays(arrayOne, arrayTwo);
+  if (result) {
+    console.log(`🚗💨💨 Assertion Passed: ${arrayOne} === ${arrayTwo}\n`);
+  } else {
+    console.log(`🤬🤬🤬 Assertion Failed: ${arrayOne} !== ${arrayTwo}\n`);
+  }
+};
+
+
+const letterPositions = function(sentanceInput) {
+  const objectResult = {};
+  for (let i = 0; i < sentanceInput.length; i++) {
+    let letterHolder = sentanceInput[i];
+    if (letterHolder !== " ") { // we want NON-space characters
+      if (objectResult[letterHolder] === undefined) { //if objectResult is missing the property then...
+        objectResult[letterHolder] = [i];
+      } else {
+        objectResult[letterHolder].push(i);
+      }
+    }
+  }
+  return objectResult;
+};
+
+console.log(letterPositions("Lighthouse in the house"));
+assertArraysEqual(letterPositions("Lighthouse in the house").L, [0]);
+//return key/value pair with an array of numbers as the property
+// use a for loop since we're dealing with indices
+// keep same conditionals to deal with whitespace/update key/value pair
+//
+
