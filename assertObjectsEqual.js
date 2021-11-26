ccdf6889c8fd8818d1a1548df3735e0a3067df4d
Michael <Michael.James.Ashworth@Gmail.com>
2021-11-26 23:21:47 +0000

    created test case to use for objects. Working + tested.

---
+++ assertObjectsEqual.js
@@ -0,0 +1,40 @@
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  } for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  for (const key in object1) {
+    if (Array.isArray(object1[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertObjectsEqual = function(object1, object2) {
+  const inspect = require('util').inspect;
+  const result = eqObjects(object1, object2);
+  if (result) {
+    console.log(`🚗💨💨 Assertion Passed:\n ${inspect(object1)} === ${inspect(object2)}\n`);
+  } else {
+    console.log(`🤬🤬🤬 Assertion Failed:\n ${inspect(object1)} !== ${inspect(object2)}\n`);
+  }
+};
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertObjectsEqual(cd, dc);
