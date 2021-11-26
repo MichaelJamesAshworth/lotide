8a5577e3937032d61a99f8e82966d3632195f0b6
Michael <Michael.James.Ashworth@Gmail.com>
2021-11-26 03:17:22 +0000

    she linted

--- findKey.js
+++ findKey.js
@@ -22,7 +22,7 @@ findKey({
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
+}, x => x.stars === 2);
 
 const result1 = findKey({
   "Blue Hill": { stars: 1 },
@@ -31,7 +31,7 @@ const result1 = findKey({
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2)
+}, x => x.stars === 2);
 
 const result2 = findKey({
   "uwu" : "huwuo",
