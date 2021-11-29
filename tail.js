e069eb0fe9484e486c5e53dfc13d3f9d0364dde1
Michael <Michael.James.Ashworth@Gmail.com>
2021-11-29 18:39:00 +0000

    refactored code, added module to refactor code of its tests

--- tail.js
+++ tail.js
@@ -1,31 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🚗💨💨 Assertion Passed: ${actual} === ${expected}\n`);
-  } else {
-    console.log(`🤬🤬🤬 Assertion Failed: ${actual} !== ${expected}\n`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const tail = function(array) {
   let newArray = array.slice(1);
   return newArray;
 };
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements since we used slice!
-//If we wanted to test the array that tail is outputting:
-assertEqual(tail(words).length, 2);
+module.exports = tail;
 
-const testTwo = ["yo yo"];
-tail(testTwo);
-assertEqual(testTwo.length, 1);
-//If we wanted to test the array that tail is outputting:
-assertEqual(tail(testTwo).length, 0);
-
-const testThree = [];
-tail(testThree);
-assertEqual(testThree.length, 0);
-//If we wanted to test the array that tail is outputting:
-assertEqual(tail(testThree).length, 0);
