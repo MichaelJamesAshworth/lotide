c81a31caa60fc3604b83bbe4c189cf8745e32d83
Michael <Michael.James.Ashworth@Gmail.com>
2021-11-29 18:48:26 +0000

    refactored code, added modules, and created test file for test code.

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🚗💨💨 Assertion Passed: ${actual} === ${expected}\n`);
-  } else {
-    console.log(`🤬🤬🤬 Assertion Failed: ${actual} !== ${expected}\n`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(arrayOne, arrayTwo) {
   if (arrayOne.length !== arrayTwo.length) {
@@ -17,11 +11,4 @@ const eqArrays = function(arrayOne, arrayTwo) {
   return true;
 };
 
-//test code
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 3]), true); // => should FAIL
-assertEqual(eqArrays([], []), true); // => should PASS
-
-//How to determine a perfect match
-//check the length of the array
-//check the indexes are the same
+module.exports = eqArrays;
