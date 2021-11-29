88088c440676da89df5ad5089215ced393f064e2
Michael <Michael.James.Ashworth@Gmail.com>
2021-11-29 22:02:44 +0000

    implemented mocha/chai testing for test file

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,21 +1,21 @@
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
+const assert = require('chai').assert;
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements since we used slice!
-//If we wanted to test the array that tail is outputting:
-assertEqual(tail(words).length, 2);
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
 
-const testTwo = ["yo yo"];
-tail(testTwo);
-assertEqual(testTwo.length, 1);
-//If we wanted to test the array that tail is outputting:
-assertEqual(tail(testTwo).length, 0);
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
 
-const testThree = [];
-tail(testThree);
-assertEqual(testThree.length, 0);
-//If we wanted to test the array that tail is outputting:
-assertEqual(tail(testThree).length, 0);
+  it("returns [] for ['Yo Yo']", () => {
+    assert.deepEqual(tail(['Yo Yo']), []);
+  });
+
+  it("returns [] for []", () => {
+    assert.deepEqual(tail(['Yo Yo']), []);
+  });
+})
